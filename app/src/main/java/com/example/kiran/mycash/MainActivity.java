9b8402968b639aa960e7b30dcce49871d53b49d4
Kiran <kbatchu2010@gmail.com>
2018-03-27 11:23:56 +0530

    Added all files

---
+++ app/src/main/java/com/example/kiran/mycash/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.kiran.mycash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+TextView tVToatalBalnce;
+//EditText et
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
